220ac41f4ac97e98453de2b1aa8dbddf0cb18456
Ing. Di Filippo <davidedifilippo@gmail.com>
2022-04-02 16:16:48 +0200

    Update second_order_bode_tracer.m

--- Second_order_term/second_order_bode_tracer.m
+++ Second_order_term/second_order_bode_tracer.m
@@ -5,17 +5,13 @@ z = 0.3;
 RE_TRINOMIO = 1-(w./wn).^2;
 IM_TRINOMIO = 2*z*(w./wn);
 
-
 TRINOMIO = RE_TRINOMIO+IM_TRINOMIO*i;
 
 GdB = 20*log10(abs(TRINOMIO));
 PH = 360/(2*pi)*angle(TRINOMIO);
 
 h = figure(1);
-
-
 title('Risposta in ampiezza o guadagno');
-
 xlabel('Log10(w)');
 ylabel('dB');
 hold on;
@@ -24,10 +20,7 @@ grid on;
 plot(log10(w), -GdB, 'r','linewidth',2);
 
 h = figure(2);
-
-
 title('Risposta di fase');
-
 xlabel('Log10(w)');
 ylabel('Fase');
 hold on;
