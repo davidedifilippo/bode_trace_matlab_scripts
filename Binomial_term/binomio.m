8676e8a2a4a133c68c3b6a3788d709191307bf25
Ing. Di Filippo <davidedifilippo@gmail.com>
2022-04-03 12:36:25 +0200

    Update binomio.m

--- Binomial_term/binomio.m
+++ Binomial_term/binomio.m
@@ -4,7 +4,9 @@ tau = 0.01;
 
 
 G = abs(1+w*tau*i);
+
 GdB = 20*log10(G);
+PH = 360/(2*pi)*angle(1+w*tau*i);
 
 h = figure(1);
 title('Risposta in ampiezza o guadagno');
@@ -14,3 +16,11 @@ hold on;
 grid on;
 
 plot(log10(w), GdB, 'b','linewidth',2);
+
+title('Risposta di fase');
+xlabel('Log10(w)');
+ylabel('Fase');
+hold on;
+grid on;
+
+plot(log10(w), PH, 'r','linewidth',2);
